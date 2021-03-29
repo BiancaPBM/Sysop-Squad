34fc120ff0698f90169cf73f2494fca9a3cc16ae
Mihneo <smihnea64@gmail.com>
2021-03-30 00:01:16 +0300

    build commit

--- sysop-squad-ui/src/app/form/form.component.ts
+++ sysop-squad-ui/src/app/form/form.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  templateUrl: './form.component.html'
+  // styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
