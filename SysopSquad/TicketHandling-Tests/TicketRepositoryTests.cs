b2e4bfb28844542521591ea4783662fbe9dc571e
mirela99 <adamache.mirela@yahoo.com>
2021-03-30 15:55:37 +0300

    Added Unit Test

---
+++ SysopSquad/TicketHandling-Tests/TicketRepositoryTests.cs
@@ -0,0 +1,34 @@
+using Moq;
+using System;
+using TicketHandling.Microservice.Data;
+using TicketHandling.Microservice.Model;
+using Xunit;
+
+namespace TicketRepositoryTests
+{
+  public class TicketRepositoryTests
+  {
+    private readonly Mock<ITicketsDatabaseSettings> _mockContext;
+    public TicketRepositoryTests()
+    {
+      _mockContext = new Mock<ITicketsDatabaseSettings>();
+    }
+
+    [Fact]
+    public async void TicketRepository_Create_NullUser_Failure()
+    {
+      // Arrange
+      Ticket _movie = null;
+
+      //Act 
+      _mockContext.Setup((p => p.TicketsCollectionName)).Returns("tickets");
+      _mockContext.Setup((p => p.ConnectionString)).Returns("mongodb://localhost:27017");
+      _mockContext.Setup((p => p.DatabaseName)).Returns("TicketHandling");
+      var ticketRepository = new TicketRepository(_mockContext.Object);
+
+      // Assert
+      await Assert.ThrowsAsync<ArgumentNullException>(() => ticketRepository.AddTicket(_movie));
+    }
+
+  }
+}
