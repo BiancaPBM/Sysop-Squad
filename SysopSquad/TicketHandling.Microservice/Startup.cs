e92c08086ec8b3814855562091119b1dd3cf74ba
mirela99 <adamache.mirela@yahoo.com>
2021-03-30 15:07:34 +0300

    Backend CRUD operation for TicketHandling API

---
+++ SysopSquad/TicketHandling.Microservice/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using TicketHandling.Microservice.Data;
+using TicketHandling.Microservice.Model;
+
+namespace TicketHandling.Microservice
+{
+  public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<TicketsDatabaseSettings>(
+                Configuration.GetSection(nameof(TicketsDatabaseSettings)));
+
+            services.AddSingleton<ITicketsDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<TicketsDatabaseSettings>>().Value);
+
+            services.AddTransient<ITicketRepository, TicketRepository>();
+          
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TicketHandling.Microservice", Version = "v1" });
+            });
+            services.AddCors(c =>
+            {
+              c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+    }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketHandling.Microservice v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod()
+                .AllowAnyHeader());
+    }
+    }
+}
